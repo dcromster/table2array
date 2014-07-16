ae57529964afd81a32b011f354c579ab2a57364c
Roman Milovskiy <dcromster@gmail.com>
2014-07-16 15:07:32 +0400

    v1

---
+++ Table2Array.pm
@@ -0,0 +1,133 @@
+package Table2Array;
+=pod
+    HTML table parser.
+    v1 16072014
+=cut
+
+use strict;
+use Data::Dumper;
+
+sub new {
+    my ($class, %self) = @_;
+    bless \%self, $class;
+    return \%self;
+}
+
+sub get_data($) {
+    # returns data in cells
+    my($self) = @_;
+    my $string = $self->{'string'};
+
+    my @table_array; # массив с данными таблицы
+    my $row_counter = 0; # счётчик рядов
+
+    $string =~ s/\n//igm;
+
+    # Getting data inside tbody
+    $string =~ m{<tbody>(.+)</tbody>}igm;
+    $string = $1;
+
+    my @rows = split(/<\/tr>/,$string);
+
+
+    foreach my $row (@rows) {
+        $row =~ s{<tr>}{}igm;
+        my @rows = split(/<\/td>/,$row);
+        my @table_array_tmp;
+
+        foreach my $tmp (@rows) {
+
+            # Splitting for cells
+            my @cells = split(/<\/td>/, $tmp);
+            my $value_counter = 0;
+            my @rows_data;
+
+            foreach my $cell (@cells) {
+                $cell =~ s{<td.*>}{}igm;
+                push (@rows_data, $cell);
+                print STDERR "$cell\n";;
+            };
+            push (@table_array_tmp, \@rows_data);
+        };
+        push (@table_array, \@table_array_tmp);
+    };
+    return \@table_array;
+}
+
+sub get_info($) {
+    my($self) = @_;
+    my $string = $self->{'string'};
+
+    # Getting data inside tbody
+    $string =~ m{<tbody>(.+)</tbody>}igm;
+    $string = $1;
+    $string =~ s{<tr>}{}igm;
+
+    # Splitting for strings
+    my @tmps = split (/<\/tr>/, $string);
+
+    my @result;
+
+    foreach my $tmp (@tmps) {
+        # Splitting for cells
+        my @cells = split(/<\/td>/, $tmp);
+        foreach my $tmp1 (@cells) {
+            # Inside cell
+            my %cell; # Cell prp result will be here
+            $tmp1 =~ m{<td(.+)>}igm;
+            my @cells_data = split(/['|"] /,$1);
+
+            foreach my $cell_data (@cells_data) {
+                # Inside cell prop
+                my ($name, $value) = split(/=/, $cell_data);
+                $value =~ s{['|"]}{}igm;
+                $name =~ s{ }{}igm;
+                $cell{$name} = $value;
+            };
+
+            push (@result, \%cell);
+        };
+
+    };
+    use Data::Dumper;
+    return \@result;
+};
+
+sub get_all() {
+    # returns tags data and data in cells
+    my($self) = @_;
+    my $string = $self->{'string'};
+
+    my %table_hash = (
+        'data' => get_data($self),
+        'info' => get_info($self)
+    );
+    return \%table_hash;
+}
+
+1;
+
+
+__END__
+
+
+=head NAME
+Table2Array - HTML table parser
+
+=head1 SYNOPSIS
+
+use Table2Array;
+
+my $html_table = 'html table code here';
+
+my $table_data = Table2Array::get_data($html_table); # returns array ref to all data in cells of $html_table between <td> or <th> and </td> or </th>
+
+my $table_tags = Table2Array::get_info($html_table); # return array ref to all tags info
+
+my $table_all = Table2Array::get_all($html_table); #return all data from table.
+
+=head1 DESCRIPTION
+
+Table2Array simple HTML table parser which return a data in cells and cell properties.
+
+=item
---
+++ Table2Array_primer.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+
+use strict;
+use Data::Dumper;
+
+use Table2Array;
+
+my $test_table = "<table><thead><tr><th class='head cell1'>head cell1</th><th class='class head cell1' id='id head cell1'>head cell2</th></tr></thead><tbody><tr><td class='class row1 cell1'>row1 cell1</td><td>row1 cell2</td><td class='class row1 cell3'>row1 cell3</td></tr><tr><td class='class row2 cell1'>row2 cell1</td><td class='class row2 cell2' another_data = \"adata\">row2 cell2</td></tr></tbody>";
+
+# getting data from cells
+my $table = new Table2Array('string' => $test_table);
+my $cells_data = $table->get_data();
+print Dumper($cells_data);
+
+#~ #getting data from cells properties
+#~ my $table = new Table2Array('string' => $test_table);
+#~ my $tags_data = $table->get_info();
+#~ print Dumper($tags_data);
+#~
+#~ # Getting all data from table
+#~ my $table = new Table2Array('string' => $test_table);
+#~ my $tags_data = $table->get_all();
+#~ print Dumper($tags_data);
